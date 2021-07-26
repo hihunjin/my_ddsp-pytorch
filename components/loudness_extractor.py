bf08ebadc1e8ebe46344153667045d3f620cfb7b
hihunjin <32363064+hihunjin@users.noreply.github.com>
2021-07-26 13:14:41 +0900

    Update loudness_extractor.py

--- components/loudness_extractor.py
+++ components/loudness_extractor.py
@@ -78,10 +78,11 @@ class LoudnessExtractor(nn.Module):
         sliced_signal = paded_input_signal.unfold(1, self.n_fft, self.frame_length)     # torch.Size([1, 250, 320])
         sliced_windowed_signal = sliced_signal * self.smoothing_window                  # torch.Size([1, 250, 320])
         
-        SLICED_SIGNAL = torch.rfft(sliced_windowed_signal, 1, onesided = False)                                         # torch.Size([1, 250, 320, 2])
+#         SLICED_SIGNAL = torch.rfft(sliced_windowed_signal, 1, onesided = False)                                         # torch.Size([1, 250, 320, 2])
         
-        SLICED_SIGNAL_LOUDNESS_SPECTRUM = torch.zeros(SLICED_SIGNAL.shape[:-1])                                         # torch.Size([1, 250, 320, 2])
-        SLICED_SIGNAL_LOUDNESS_SPECTRUM = SLICED_SIGNAL[:, :, :, 0] ** 2 + SLICED_SIGNAL[:, :, :, 1] ** 2               # torch.Size([1, 250, 320])
+#         SLICED_SIGNAL_LOUDNESS_SPECTRUM = torch.zeros(SLICED_SIGNAL.shape[:-1])                                         # torch.Size([1, 250, 320, 2])
+#         SLICED_SIGNAL_LOUDNESS_SPECTRUM = SLICED_SIGNAL[:, :, :, 0] ** 2 + SLICED_SIGNAL[:, :, :, 1] ** 2               # torch.Size([1, 250, 320])
+        SLICED_SIGNAL_LOUDNESS_SPECTRUM = torch.fft.fft(sliced_windowed_signal).abs() ** 2
                 
         freq_bin_size = self.sr / self.n_fft
         FREQUENCIES = torch.tensor([(freq_bin_size * i) % (0.5 * self.sr) for i in range(self.n_fft)]).to(self.device)
